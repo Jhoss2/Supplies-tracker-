711e6fc120013eb7705c3fa2319dd521f14b60f0
Jhoss2 <Jhoss8631@gmail.com>
2026-02-20 09:51:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('SuppliesTracker', () => App);
+import {name as appName} from './app.json'; // Ceci va chercher "SuppliesTracker" automatiquement
+
+AppRegistry.registerComponent(appName, () => App);
