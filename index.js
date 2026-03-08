75742b7eb3bd2b6aaba62c68dd0f23716ec116c2
Jhoss2 <Jhoss8631@gmail.com>
2026-03-08 23:24:03 +0000

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-import {AppRegistry} from 'react-native';
-import App from './App';
-
-// Le nom ci-dessous DOIT être identique au "name" dans app.json
-AppRegistry.registerComponent('SuppliesTracker', () => App);
